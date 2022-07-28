3c25343f23272d941bc4d058a1b093c4134c23c9
小野 和也 <prvogo@gmail.com>
2022-07-29 07:40:07 +0900

    【docusaurus】トップページの編集

--- documents/src/components/HomepageFeatures/index.tsx
+++ documents/src/components/HomepageFeatures/index.tsx
@@ -2,56 +2,37 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
+// docusaurus
+import Link from '@docusaurus/Link';
+
+// i18n
+import Translate from '@docusaurus/Translate';
+
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Png: string;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    title: 'Conditionals',
+    Png: require('@site/static/img/utils/IS.png').default,
+    description: <Translate>A collection of simple IS functions.</Translate>,
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Png, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <Link className={clsx('col col--6')} to="/docs/category/conditionals">
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={Png} style={{ maxWidth: '384px' }} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
-    </div>
+    </Link>
   );
 }
 
@@ -59,7 +40,7 @@ export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className="row" style={{ justifyContent: 'center' }}>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
